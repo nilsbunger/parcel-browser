0dda759e18e03b715ffcd9671f360e5816c4c5a1
richiboi <61484420+richiboi@users.noreply.github.com>
2022-07-20 03:41:10 +0800

    Added multiprocessing to dataprep and parcel scripts. Efficiency 🚀 (#33)
    * Added parallel computation of parcels dataprep
    
    * Added multiprocessing for parcel script

--- world/management/commands/dataprep.py
+++ world/management/commands/dataprep.py
@@ -3,7 +3,7 @@ import django
 from django.contrib.gis.db.models import Extent
 
 from lib.crs_lib import get_utm_crs
-from lib.topo_lib import calculate_parcel_slopes, check_topos_for_parcels
+from lib.topo_lib import calculate_parcel_slopes, calculate_parcel_slopes_mp, check_topos_for_parcels
 from django.core.management.base import BaseCommand
 
 from world.models import Parcel
@@ -12,6 +12,8 @@ from world.models import Parcel
 class Neighborhood(Enum):
     # Mira Mesa neighborhood of San Diego
     Miramesa = -117.17987773162996, 32.930825570911985, -117.12513392170659, 32.894946222075184
+    MiramesaSmall = (-117.135284737197, 32.905422120627904, -
+                     117.13317320050437, 32.90428935023001)
 
     # Special "neighborhood" - compute full extents of all parcels
     all = tuple()
@@ -45,5 +47,10 @@ class Command(BaseCommand):
             check_topos_for_parcels(bounding_box)
         else:
             print(f'Calculating slopes for parcels in {hood} neighbordhood(s)')
-            calculate_parcel_slopes(bounding_box, sd_utm_crs)
+
+            # Put behind feature flag in case parallel breaks
+            if True:
+                calculate_parcel_slopes_mp(bounding_box, sd_utm_crs)
+            else:
+                calculate_parcel_slopes(bounding_box, sd_utm_crs)
             self.stdout.write(self.style.SUCCESS(f'Finished calculating parcel slopes for neighborhood {hood}'))
