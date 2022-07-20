1a274e6f5c43bae2654a2d14d17011ced8d5d301
Soham <sohvasa@gmail.com>
2022-07-20 09:27:34 -0700

    Fix library path for linuxbrew

--- mygeo/settings.py
+++ mygeo/settings.py
@@ -85,8 +85,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mygeo.wsgi.application'
 
-GDAL_LIBRARY_PATH = '/opt/homebrew/lib/libgdal.dylib'
-GEOS_LIBRARY_PATH = '/opt/homebrew/lib/libgeos_c.dylib'
+GDAL_LIBRARY_PATH = '/home/linuxbrew/.linuxbrew/lib/libgdal.so.31.0.1'
+GEOS_LIBRARY_PATH = '/home/linuxbrew/.linuxbrew/lib/libgeos_c.so.1.17.0'
+
+
+# GDAL_LIBRARY_PATH = '/opt/homebrew/lib/libgdal.dylib'
+# GEOS_LIBRARY_PATH = '/opt/homebrew/lib/libgeos_c.dylib'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
