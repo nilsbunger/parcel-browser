cfaddf8b1a9d0fef579562981db24e99ff29d70e
Richard Cheung <richard.yh.cheung@gmail.com>
2022-08-01 14:38:19 +0800

    Implemented initial table view for listings

--- frontend/src/pages/ListingsPage.tsx
+++ frontend/src/pages/ListingsPage.tsx
@@ -1,17 +1,113 @@
 import useSWR from 'swr';
-import React from 'react';
+import React, { useState } from 'react';
+import {
+    createColumnHelper,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+} from '@tanstack/react-table';
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+function snakeCaseToTitleCase(word: string) {
+    const tokenized = word.toLowerCase().split('_');
+    for (let i = 0; i < tokenized.length; i++) {
+        tokenized[i] = tokenized[i][0].toUpperCase() + tokenized[i].slice(1);
+    }
+
+    return tokenized.join(' ');
+}
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+const columnHelper = createColumnHelper();
 
 export function ListingsPage() {
-    const { data, error } = useSWR('/api/user', fetcher);
+    const { data, error } = useSWR('/dj/api/listings', fetcher);
+    const [columnVisibility, setColumnVisibility] = useState({});
+
+    const columns = data
+        ? data.schema.fields.map((field) =>
+              columnHelper.accessor(field.name, {
+                  header: snakeCaseToTitleCase(field.name),
+              })
+          )
+        : [];
+
+    const table = useReactTable({
+        data: data ? data.data : [],
+        columns,
+        state: {
+            columnVisibility,
+        },
+        onColumnVisibilityChange: setColumnVisibility,
+        getCoreRowModel: getCoreRowModel(),
+    });
 
     if (error) return <div>failed to load</div>;
     if (!data) return <div>loading...</div>;
 
+    console.log(columnVisibility);
+    console.log(table.getRowModel());
     return (
         <>
             <h1>Hello world from pages/ListingsPage.tsx!</h1>
+            <label>
+                <input
+                    {...{
+                        type: 'checkbox',
+                        checked: table.getIsAllColumnsVisible(),
+                        onChange: table.getToggleAllColumnsVisibilityHandler(),
+                    }}
+                />{' '}
+                Toggle All
+            </label>
+            {table.getAllLeafColumns().map((column) => {
+                return (
+                    <div key={column.id} className="px-1">
+                        <label>
+                            <input
+                                {...{
+                                    type: 'checkbox',
+                                    checked: column.getIsVisible(),
+                                    onChange:
+                                        column.getToggleVisibilityHandler(),
+                                }}
+                            />{' '}
+                            {column.id}
+                        </label>
+                    </div>
+                );
+            })}
+            <table>
+                <thead>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => (
+                                <th key={header.id}>
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                              header.column.columnDef.header,
+                                              header.getContext()
+                                          )}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody>
+                    {table.getRowModel().rows.map((row) => (
+                        <tr key={row.id}>
+                            {row.getVisibleCells().map((cell) => (
+                                <td key={cell.id}>
+                                    {flexRender(
+                                        cell.column.columnDef.cell,
+                                        cell.getContext()
+                                    )}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
     );
 }
