176d10289de938822b84679640ba94675c5885d0
Richard Cheung <richard.yh.cheung@gmail.com>
2022-08-01 10:34:43 +0800

    Formatted urls.py

--- mygeo/urls.py
+++ mygeo/urls.py
@@ -16,11 +16,14 @@ urlpatterns = [
     path('dj/parcel/<str:apn>', ParcelDetailView.as_view()),
 
     # Django-generated API routes
-    path('dj/api/parceltile/<int:z>/<int:x>/<int:y>', views.ParcelTileData.as_view(), name="parcel-tile"),
-    path('dj/api/topotile/<int:z>/<int:x>/<int:y>', views.TopoTileData.as_view(), name="topo-tile"),
+    path('dj/api/parceltile/<int:z>/<int:x>/<int:y>',
+         views.ParcelTileData.as_view(), name="parcel-tile"),
+    path('dj/api/topotile/<int:z>/<int:x>/<int:y>',
+         views.TopoTileData.as_view(), name="topo-tile"),
     path('dj/api/listings', views.ListingsData.as_view(), name="listings"),
     path('dj/parcel/<str:apn>/geodata', ParcelDetailData.as_view()),
-    path('dj/parcel/<str:apn>/geodata/neighbor', IsolatedNeighborDetailData.as_view()),
+    path('dj/parcel/<str:apn>/geodata/neighbor',
+         IsolatedNeighborDetailData.as_view()),
 
     # All other routes - send to React for rendering
     re_path(r'^(.*)$', frontend_proxy_view),
