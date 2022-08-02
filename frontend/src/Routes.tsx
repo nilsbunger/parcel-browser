79be16e330973be81644559d6cf29306aee795b3
Richard Cheung <richard.yh.cheung@gmail.com>
2022-08-02 15:07:47 +0800

    Format routes.tsx

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,30 +1,29 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {HomePage} from "./pages/HomePage";
-import HomeLayout from "./layouts/HomeLayout";
-import * as React from "react";
-import {ListingsPage} from "./pages/ListingsPage";
-import {ListingDetailPage} from "./pages/ListingDetailPage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import HomeLayout from './layouts/HomeLayout';
+import * as React from 'react';
+import { ListingsPage } from './pages/ListingsPage';
+import { ListingDetailPage } from './pages/ListingDetailPage';
 
 export function MyRoutes() {
-    return (
-        <BrowserRouter>
-            <React.StrictMode>
-
-                <Routes>
-                    <Route element={<HomeLayout/>}>
-                        <Route path="/" >
-                            <Route index element={<HomePage/>} />
-                            <Route path="listings">
-                                <Route index element={<ListingsPage/>}/>
-                                <Route path=":listingId" element={<ListingDetailPage/>} />
-                            </Route>
-                        </Route>
-                    </Route>
-                    <Route path="*" element={<PageNotFound/>}/>
-                </Routes>
-            </React.StrictMode>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <React.StrictMode>
+        <Routes>
+          <Route element={<HomeLayout />}>
+            <Route path="/">
+              <Route index element={<HomePage />} />
+              <Route path="listings">
+                <Route index element={<ListingsPage />} />
+                <Route path=":listingId" element={<ListingDetailPage />} />
+              </Route>
+            </Route>
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </React.StrictMode>
+    </BrowserRouter>
+  );
 }
 
 function PageNotFound() {
