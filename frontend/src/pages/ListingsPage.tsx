84defb3c9569493ae461b1419633e6e51a5b4717
Richard Cheung <richard.yh.cheung@gmail.com>
2022-08-02 14:59:22 +0800

    Added sorting on table fields

--- frontend/src/pages/ListingsPage.tsx
+++ frontend/src/pages/ListingsPage.tsx
@@ -5,6 +5,8 @@ import {
   flexRender,
   getCoreRowModel,
   useReactTable,
+  getSortedRowModel,
+  SortingState,
 } from '@tanstack/react-table';
 import { Link } from 'react-router-dom';
 import { fetcher } from '../utils/fetcher';
@@ -21,69 +23,71 @@ function snakeCaseToTitleCase(word: string) {
 const columnHelper = createColumnHelper();
 
 const initialColumnState = {
-    "apn": true,
-    "address": true,
-    "zone": true,
-    "num_existing_buildings": false,
-    "is_flag_lot": false,
-    "carports": false,
-    "garages": false,
-    "parcel_size": true,
-    "existing_living_area": false,
-    "existing_floor_area": false,
-    "existing_FAR": true,
-    "num_new_buildings": false,
-    "new_building_areas": false,
-    "total_added_building_area": false,
-    "garage_con_units": false,
-    "garage_con_area": false,
-    "total_new_units": false,
-    "total_added_area": false,
-    "new_FAR": false,
-    "max_FAR": true,
-    "potential_FAR": true,
-    "limiting_factor": false,
-    "main_building_poly_area": false,
-    "accessory_buildings_polys_area": false,
-    "avail_geom_area": false,
-    "avail_area_by_FAR": true,
-    "parcel_sloped_area": false,
-    "parcel_sloped_ratio": false,
-    "total_score": false,
-    "cap_ratio_score": false,
-    "open_space_score": false,
-    "project_size_score": false,
-    "can_lot_split": false,
-    "new_lot_area_ratio": false,
-    "new_lot_area": false,
-    "git_commit_hash": false,
-    "datetime_ran": false,
-    "front_setback": false,
-    "bedrooms": true,
-    "bathrooms": true,
-    "price": true,
-    "zipcode": false,
-    "founddate": false,
-    "seendate": false,
-    "mlsid": false,
-    "mls_floor_area": false,
-    "thumbnail": false,
-    "listing_url": false,
-    "soldprice": false,
-    "status": true
-}
+  apn: true,
+  address: true,
+  zone: true,
+  num_existing_buildings: false,
+  is_flag_lot: false,
+  carports: false,
+  garages: false,
+  parcel_size: true,
+  existing_living_area: false,
+  existing_floor_area: false,
+  existing_FAR: true,
+  num_new_buildings: false,
+  new_building_areas: false,
+  total_added_building_area: false,
+  garage_con_units: false,
+  garage_con_area: false,
+  total_new_units: false,
+  total_added_area: false,
+  new_FAR: false,
+  max_FAR: true,
+  potential_FAR: true,
+  limiting_factor: false,
+  main_building_poly_area: false,
+  accessory_buildings_polys_area: false,
+  avail_geom_area: false,
+  avail_area_by_FAR: true,
+  parcel_sloped_area: false,
+  parcel_sloped_ratio: false,
+  total_score: false,
+  cap_ratio_score: false,
+  open_space_score: false,
+  project_size_score: false,
+  can_lot_split: false,
+  new_lot_area_ratio: false,
+  new_lot_area: false,
+  git_commit_hash: false,
+  datetime_ran: false,
+  front_setback: false,
+  bedrooms: true,
+  bathrooms: true,
+  price: true,
+  zipcode: false,
+  founddate: false,
+  seendate: false,
+  mlsid: false,
+  mls_floor_area: false,
+  thumbnail: false,
+  listing_url: false,
+  soldprice: false,
+  status: true,
+};
 
-function roundIfNumber(val:any): any {
-    if (typeof val == "number") {
-        console.log (val + " is a number")
-        return val.toPrecision(3)
-    }
-    return val
+function roundIfNumber(val: any): any {
+  if (typeof val == 'number') {
+    console.log(val + ' is a number');
+    return val.toPrecision(3);
+  }
+  return val;
 }
 
 export function ListingsPage() {
   const { data, error } = useSWR('/dj/api/listings', fetcher);
-  const [columnVisibility, setColumnVisibility] = useState(initialColumnState);
+  const [columnVisibility, setColumnVisibility] =
+    useState<Record<string, boolean>>(initialColumnState);
+  const [sorting, setSorting] = React.useState<SortingState>([]);
 
   const columns = data
     ? data.schema.fields.map((field) =>
@@ -97,8 +101,8 @@ export function ListingsPage() {
                   </Link>
                 )
               : ['price', 'bedrooms', 'bathrooms'].includes(field.name) // fields which should not get rounded
-                ? ({ row }) => row.getValue(field.name)
-                : ({ row }) => roundIfNumber(row.getValue(field.name)),
+              ? ({ row }) => row.getValue(field.name)
+              : ({ row }) => roundIfNumber(row.getValue(field.name)),
         })
       )
     : [];
@@ -108,9 +112,12 @@ export function ListingsPage() {
     columns,
     state: {
       columnVisibility,
+      sorting,
     },
     onColumnVisibilityChange: setColumnVisibility,
+    onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
   });
 
   if (error) return <div>failed to load</div>;
@@ -126,12 +133,25 @@ export function ListingsPage() {
             <tr key={headerGroup.id}>
               {headerGroup.headers.map((header) => (
                 <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
+                  {header.isPlaceholder ? null : (
+                    <div
+                      {...{
+                        className: header.column.getCanSort()
+                          ? 'cursor-pointer select-none'
+                          : '',
+                        onClick: header.column.getToggleSortingHandler(),
+                      }}
+                    >
+                      {flexRender(
                         header.column.columnDef.header,
                         header.getContext()
                       )}
+                      {{
+                        asc: ' 🔼',
+                        desc: ' 🔽',
+                      }[header.column.getIsSorted() as string] ?? null}
+                    </div>
+                  )}
                 </th>
               ))}
             </tr>
@@ -149,7 +169,7 @@ export function ListingsPage() {
           ))}
         </tbody>
       </table>
-              <label>
+      <label>
         <input
           {...{
             type: 'checkbox',
@@ -175,7 +195,6 @@ export function ListingsPage() {
           </div>
         );
       })}
-
     </>
   );
 }
