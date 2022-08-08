9f747fdf166e15c940d63094e2e7940467b76a05
Nils Bunger <nilsbunger@gmail.com>
2022-08-08 13:17:35 -0700

    scraper small fixes to match more parcels

--- lib/listings_lib.py
+++ lib/listings_lib.py
@@ -7,11 +7,12 @@ from world.models import PropertyListing, Parcel
 
 street_suffixes = [
     'dr', 'drive', 'way', 'ave', 'avenue', 'ct', 'court', 'blvd', 'boulevard', 'st', 'street', 'pl', 'place', 'rd', 'road', 'ln',
-    'lane', 'cove', 'cv', 'cir', 'circle', 'glen', 'gln', 'w', 'loop', 'trail', 'trl', 'ridge', 'rdg', 'highway', 'hwy',
-    'parkway', 'pkwy', 'terrace', 'ter', 'terr']
+    'lane', 'cove', 'cv', 'cir', 'circle', 'glen', 'gln', 'w', 'loop', 'point', 'pt', 'trail', 'trl', 'ridge', 'rdg', 'highway', 'hwy',
+    'parkway', 'pkwy', 'row', 'terrace', 'ter', 'terr', 'bend']
 
 normalize_suffix = {
     'avenue': 'ave',
+    'bend': 'bnd',
     'boulevard': 'blvd',
     'circle': 'cir',
     'court': 'ct',
@@ -20,6 +21,7 @@ normalize_suffix = {
     'highway': 'hwy',
     'lane': 'ln',
     'place': 'pl',
+    'point': 'pt',
     'road': 'rd',
     'parkway': 'pkwy',
     'ridge': 'rdg',
@@ -55,7 +57,7 @@ def address_to_parcel(addr: str, neighborhood: str = None) -> (Parcel | None, st
         rest[0] = 'mt'
     if rest[0] == 'saint':
         rest[0] = 'st'
-    if len(rest) > 1:
+    if len(rest) > 2:
         if rest[0] in ['south', 'north', 'east', 'west', 'n', 'w', 'e', 's']:
             street_prefix = normalize_prefix.get(rest[0], rest[0])
             rest = rest[1:]
@@ -75,15 +77,26 @@ def address_to_parcel(addr: str, neighborhood: str = None) -> (Parcel | None, st
     if hyphenated_addr_num:
         print(f"Hyphenated address -- {addr_normalized}")
         addr_num = hyphenated_addr_num.groups()[0]
+
     try:
-        parcels = Parcel.objects.filter(
+        parcels_exact = Parcel.objects.filter(
+            situs_addr=addr_num, situs_stre__iexact=street_name,)
+        parcels_inexact = Parcel.objects.filter(
             situs_addr=addr_num, situs_stre__istartswith=street_name,)
     except Exception as e:
         print(f"Error processing {addr_normalized}")
         return None, 'dberror'
-    matched_parcel = None
-    if len(parcels) > 1:
-        # more than one match using street name -- see if the street suffix ('way', 'rd', ...) disambiguates it
+    # repeat loop twice, looking at exact match first.
+    for parcels in [parcels_exact, parcels_inexact]:
+        matched_parcel = None
+        if len(parcels) == 1:
+            # happy path - exact match. get out of here.
+            return parcels[0], None
+        elif len(parcels) == 0:
+            # no match, but continue to second loop if needed
+            continue
+        # more than one match -- see if the street suffix ('way', 'rd', ...) or jurisdiction ('SD') disambiguates it
+        # We track jurisdition because if we DO match in another jurisdiction, we know we didn't miss anything.
         matched_parcel_candidates = list()
         matched_jurisdiction_candidates = list()
         jurisdictions = set()
@@ -94,22 +107,18 @@ def address_to_parcel(addr: str, neighborhood: str = None) -> (Parcel | None, st
             if p.situs_juri == 'SD':
                 matched_jurisdiction_candidates.append(p)
         if len(matched_jurisdiction_candidates) == 1:
-            matched_parcel = matched_jurisdiction_candidates[0]
+            return matched_jurisdiction_candidates[0], None
         elif len(matched_jurisdiction_candidates) == 0:
             return None, 'match_out_of_jurisdiction'
         elif len(matched_parcel_candidates) == 1 and matched_parcel_candidates[0].situs_juri == 'SD':
-            matched_parcel = matched_parcel_candidates[0]
+            return matched_parcel_candidates[0], None
         else:
             print(f"Multiple matches ({len(parcels)}) for {addr_normalized} in jurisdiction SD")
             return None, f'multimatch_{jurisdictions}'
-    elif len(parcels) == 0:
-        if hyphenated_addr_num:
-            # Need to build out this case: we found a hyphenated address, but the first address didn't work.
-            # Maybe another address in the hyphen range would?
-            raise NotImplementedError("hyphenated address and didn't find it")
-        print(f"No match in Parcel table for {addr_normalized}, {neighborhood}")
-        return None, 'unmatched'
-    else:
-        # Found exactly one match, that's good
-        matched_parcel = parcels[0]
-    return matched_parcel, None
+    # After exact and inexact attempts, we're still here, and nothing matched.
+    if hyphenated_addr_num:
+        # Need to build out this case: we found a hyphenated address, but the first address didn't work.
+        # Maybe another address in the hyphen range would?
+        raise NotImplementedError("hyphenated address and didn't find it")
+    print(f"No match in Parcel table for {addr_normalized}, {neighborhood}")
+    return None, 'unmatched'
