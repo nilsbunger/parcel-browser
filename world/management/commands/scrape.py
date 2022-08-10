dac4756e8c141a2a1f56deebfb1ea45e8b0d78f7
Nils Bunger <nilsbunger@gmail.com>
2022-08-09 23:33:59 -0700

    add sraping of single zip; adopt proper django logging in scraper (#60)

--- world/management/commands/scrape.py
+++ world/management/commands/scrape.py
@@ -1,26 +1,25 @@
 from __future__ import annotations
 
+from collections import defaultdict
+from datetime import date
 import pprint
 import random
 import sys
-import os
-from collections import defaultdict
-from datetime import date
-
+import logging
 from django.core.management import BaseCommand
 from pandas import DataFrame
 
 from lib.analyze_parcel_lib import analyze_batch
 from lib.crs_lib import get_utm_crs
 from lib.listings_lib import listing_to_parcel
-from lib.neighborhoods import Neighborhood, AllSdCityZips
+from lib.neighborhoods import AllSdCityZips, Neighborhood
 from lib.scraping_lib import scrape_san_diego_listings_by_zip_groups
 from mygeo.util import eprint
 from world.models import PropertyListing
 
 
 # NOTE: We should rename this command to 'listings', since it generally does ingest operations on MLS listings
-def zip_groups_by_neighborhood(self):
+def zip_groups_by_neighborhood():
     neighborhood_groups = [[Neighborhood.SDSU, Neighborhood.Miramesa],
                            [Neighborhood.Clairemont, Neighborhood.OceanBeach],
                            [Neighborhood.Encanto, Neighborhood.AlliedGardens],
@@ -58,42 +57,42 @@ class Command(BaseCommand):
             '--fetch', action='store_true', help="Fetch listings data from MLS service"
         )
         parser.add_argument(
+            '--fetch-zip',
+            action='store', help="Fetch listings for ONE specified zip from cache (if available) or MLS service"
+        )
+        parser.add_argument(
             '--fetch-local', action='store_true',
             help="Fetch listings from localhost. Useful to see request stream, but it won't generate useful responses"
         )
         parser.add_argument(
-            '--fetch-test',
-            action='store_true',
-            help="Fetch listings for ONE zip from cache (default) or MLS service for testing"
+            '--verbose', action='store_true', help="Do verbose logging (DEBUG-level logging)"
         )
         parser.add_argument(
-            '--no-cache', action='store_true', help="Don't cache existing query data"
+            '--no-cache', action='store_true', help="Don't use existing cached query data even if it's available"
         )
         parser.add_argument(
             '--skip-analysis', action='store_true', help="Don't run parcel analysis"
         )
 
     def handle(self, *args, **options):
+        logging.getLogger().setLevel('DEBUG' if options['verbose'] else 'INFO')
+        logging.debug("log level set")
+
         # -----
         # 1. Scrape latest listings if directed to.
         # -----
-        if options['fetch'] or options['fetch_local'] or options['fetch_test']:
-            if options['fetch_test']:
-                # fetch one Clairemont zip code
-                zip_groups = [[92117]]
-            else:
-                # Fetch all zip code groups
-                # zip_groups = zip_groups_by_neighborhood()
-                zip_groups = zip_groups_from_zips(AllSdCityZips)
 
-            print(f'Fetching {len(zip_groups)}. Local={options["fetch_local"] is True}, Cache={options["no_cache"] is False}')
+        if options['fetch'] or options['fetch_local'] or options['fetch_zip']:
+            zip_groups = [[options['fetch_zip']]] if options['fetch_zip'] else zip_groups_from_zips(AllSdCityZips)
+            logging.info(
+                f'Fetching {len(zip_groups)} groups of zip codes. Local={options["fetch_local"] is True}, Cache={options["no_cache"] is False}')
             stats = scrape_san_diego_listings_by_zip_groups(
                 zip_groups, localhost_mode=options['fetch_local'], cache=not options['no_cache'])
 
-            print(f'\nCRAWLER DONE.\nFound {stats.get_value("listing/no_change")} entries with no change, '
-                  f' {stats.get_value("listing/new_or_update")} new or updated')
-            print(f'{stats.get_value("response_received_count")} responses received'
-                  f' (of which, {stats.get_value("httpcache/hit")} were from CACHE)')
+            logging.info(f'\nCRAWLER DONE.\nFound {stats.get_value("listing/no_change")} entries with no change, '
+                         f' {stats.get_value("listing/new_or_update")} new or updated')
+            logging.info(f'{stats.get_value("response_received_count")} responses received'
+                         f' (of which, {stats.get_value("httpcache/hit")} were from CACHE)')
             error_pages = stats.get_value('httperror/response_ignored_count')
             sys.stdout.flush()
             if error_pages:
@@ -102,7 +101,6 @@ class Command(BaseCommand):
         # -----
         # 2. Associate listings with parcels
         # -----
-
         # Associate parcel IDs where possible
         listings = PropertyListing.objects.filter(
             status__in=['ACTIVE', 'OFFMARKET'])
@@ -110,7 +108,7 @@ class Command(BaseCommand):
         # A set of tuples (parcel, listing)
         parcels_to_analyze = set()
         stats = defaultdict(int)
-        print(f'Found {len(listings)} properties to associate')
+        logging.info(f'Found {len(listings)} properties to associate')
         for l in listings:
             # print(f"Working on {l.addr}")
             matched_parcel, error = listing_to_parcel(l)
@@ -128,7 +126,7 @@ class Command(BaseCommand):
                     if zipcode:
                         stats[f'info_sd_{zipcode[0:5]}'] += 1
                         if int(zipcode[0:5]) not in AllSdCityZips:
-                            stats[f'error_city_zip_{zipcode[0:5]}_missing'] +=1
+                            stats[f'error_city_zip_{zipcode[0:5]}_missing'] += 1
                     else:
                         stats[f'info_sd_unknown_zip'] += 1
                 else:
@@ -136,19 +134,20 @@ class Command(BaseCommand):
                         if int(zipcode[0:5]) in AllSdCityZips:
                             # print(f"Skipping {matched_parcel.situs_addr} {matched_parcel.situs_stre}, {zip[0:5]}."
                             #       f"It's in jurisdiction={matched_parcel.situs_juri}, NOT in SD City")
-                            stats[f'error_city_zip_with_non_city_jurisdiction_{zipcode[0:5]}_{matched_parcel.situs_juri}'] += 1
+                            stats[
+                                f'error_city_zip_with_non_city_jurisdiction_{zipcode[0:5]}_{matched_parcel.situs_juri}'] += 1
                         else:
                             stats[f'info_skipping_non_city_zip{zipcode[0:5]}_{matched_parcel.situs_juri}'] += 1
             # print("SAVED")
-        print("DONE. Final stats associating parcels with listings:")
+        logging.info("DONE. Final stats associating parcels with listings:")
         pprint.pprint(dict(stats))
 
+        # -----
+        # 3. Generate parcel analysis for all the parcels if directed to
+        # -----
         if options['skip_analysis']:
-            print("SKIPPING parcel analysis")
+            logging.info("SKIPPING parcel analysis")
         else:
-            # -----
-            # 3. Generate parcel analysis for all the parcels
-            # -----
             parcels, parcel_listings = zip(*parcels_to_analyze)
             sd_utm_crs = get_utm_crs()
             results, errors = analyze_batch(
@@ -160,5 +159,5 @@ class Command(BaseCommand):
             error_df.to_csv(
                 "./frontend/static/temp_computed_imgs/errors.csv", index=False)
 
-            print(
+            logging.info(
                 f"Analysis done! There are {len(results)} successes and {len(errors)} errors.")
