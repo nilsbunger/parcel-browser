baf5463ef5a8641bb662fceeafa475eac1bba83e
Nils Bunger <nilsbunger@gmail.com>
2022-09-12 15:39:16 -0700

    add eslint, change typescript to strict. Can run eslint with yarn eslint .

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,27 @@
+// Run lint with 'yarn eslint .'
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+    // See also configuration instructions in  https://typescript-eslint.io/docs/linting/configs
+  extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      //"plugin:@typescript-eslint/strict",
+      'prettier',
+
+
+  ],
+  rules: {
+      '@typescript-eslint/restrict-template-expressions': "off",
+      '@typescript-eslint/no-unnecessary-type-assertion': "off",
+  }
+};
+
+
