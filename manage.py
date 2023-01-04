166854db13bea16b5e3d2d21bbd3f6d448a34938
Nils Bunger <nilsbunger@gmail.com>
2023-01-03 20:20:11 -0800

    always generate traceback for management commands

--- manage.py
+++ manage.py
@@ -15,6 +15,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    if len(sys.argv) > 1:
+        sys.argv.append("--traceback")  # Nils: always generate a traceback for mgmt commands
     execute_from_command_line(sys.argv)
 
 
