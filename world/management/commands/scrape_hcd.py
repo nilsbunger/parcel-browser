c0221529f3dd5fb9acf0664f59e2c13854bb4765
Nils Bunger <nilsbunger@gmail.com>
2023-01-11 13:50:45 -0800

    scrape_hcd add --no-emails option, make --try-exception cmd name consistent

--- world/management/commands/scrape_hcd.py
+++ world/management/commands/scrape_hcd.py
@@ -16,23 +16,29 @@ class Command(BaseCommand):
             help="Get all the data but print it to the console instead of updating Airtable.",
         )
         parser.add_argument(
-            "--try_exception",
+            "--no-emails",
+            action="store_true",
+            help="Skip emailing.",
+        )
+        parser.add_argument(
+            "--try-exception",
             action="store_true",
             help="Instead of doing something useful, raise an exception to make sure we get alerted.",
         )
         super().add_arguments(parser)
 
-    def handle(self, dry_run, try_exception, *args, **kwargs) -> None:
+    def handle(self, dry_run, no_emails, try_exception, *args, **kwargs) -> None:
         if dry_run:
             print("Dry run: not updating Airtable.")
         if try_exception:
             raise Exception("This is a test exception in scrape_hcd.py")
         changeSummary = run_scrape_hcd(dry_run)
-        
+
         email_subs_raw = env("HCD_EMAIL_SUBS")
         email_subs = email_subs_raw.split(",")
 
-        if not dry_run:
+        send_emails = not no_emails
+        if not dry_run and send_emails:
             email = EmailMessage(
                 subject="HCD daily update summary",
                 body=changeSummary,
@@ -43,14 +49,14 @@ class Command(BaseCommand):
             )
             email.send()
         else:
-            print("DRY RUN: NOT SENDING TO LIST:" + ",".join(email_subs))
-            
-        email = EmailMessage(
-            subject="HCD daily update summary (admin)" + (" (dry run)" if dry_run else ""),
-            body="Sent to " + ",".join(email_subs) + "\n\n" + changeSummary,
-            from_email="marcio@home3.co",
-            to=["marcio@home3.co"] if dry_run else "founders@home3.co",
-            cc=[],
-            bcc=[],
-        )
-        email.send() 
+            print("SKIPPING EMAILS: NOT SENDING TO LIST:" + ",".join(email_subs))
+        if send_emails:
+            email = EmailMessage(
+                subject="HCD daily update summary (admin)" + (" (dry run)" if dry_run else ""),
+                body="Sent to " + ",".join(email_subs) + "\n\n" + changeSummary,
+                from_email="marcio@home3.co",
+                to=["marcio@home3.co"] if dry_run else "founders@home3.co",
+                cc=[],
+                bcc=[],
+            )
+            email.send()
