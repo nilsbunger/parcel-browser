ca4d1a565ca94f4e90fdf00f3e1d7d388b38ea2d
Nils Bunger <nilsbunger@gmail.com>
2023-01-14 08:45:38 -0800

    add cc and bcc support to mailersend backend

--- world/email/mailersend_backend.py
+++ world/email/mailersend_backend.py
@@ -29,12 +29,14 @@ class MailerSendBackend(BaseEmailBackend):
         mailer = emails.NewEmail(mailersend_api_key=self.api_key)
         assert len(email_messages) == 1, "We only support sending one email at a time at the moment"
         email_message = email_messages[0]
-        assert len(email_message.cc) == 0, "We don't support cc at the moment"
-        assert len(email_message.bcc) == 0, "We don't support bcc at the moment"
         mail_body = {}  # dict in which mail content gets stored by next lines
         mailer.set_mail_from({"name": email_message.from_email, "email": email_message.from_email}, mail_body)
         mailer_to_list = [{"name": to_item, "email": to_item} for to_item in email_message.to]
+        mailer_cc_list = [{"name": cc_item, "email": cc_item} for cc_item in email_message.cc]
+        mailer_bcc_list = [{"name": bcc_item, "email": bcc_item} for bcc_item in email_message.bcc]
         mailer.set_mail_to(mailer_to_list, mail_body)
+        mailer.set_cc_recipients(mailer_cc_list, mail_body)
+        mailer.set_bcc_recipients(mailer_bcc_list, mail_body)
         mailer.set_subject(email_message.subject, mail_body)
         mailer.set_plaintext_content(email_message.body, mail_body)
         # TODO: Support HTML content
@@ -42,5 +44,5 @@ class MailerSendBackend(BaseEmailBackend):
         code, messages = resp.split("\n")
         if int(code) > 299:
             raise Exception(messages)
-        print("Sent email to", email_message.to)
+        print(f"Sent email to:{email_message.to}, cc:{email_message.cc}, bcc:{email_message.bcc}")
         return resp
