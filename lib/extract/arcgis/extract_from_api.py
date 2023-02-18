6382ade2f7741787529b7c60e840c09373eaa84f
Nils Bunger <nilsbunger@gmail.com>
2023-02-17 22:15:38 -0800

    elt: remove vestigial code

--- lib/extract/arcgis/extract_from_api.py
+++ lib/extract/arcgis/extract_from_api.py
@@ -1,11 +1,7 @@
 from copy import deepcopy
-from datetime import datetime
 import json
 import traceback
-from zoneinfo import ZoneInfo
-
 import requests
-from rich.prompt import Prompt
 
 from elt.models import RawSantaAnaParcel
 from lib.extract.arcgis.types import GeoEnum, GisDataTypeEnum
@@ -145,7 +141,9 @@ def parcel_data_fetcher(url, arc_custom_params, *, do_incremental, thru_data, sk
         objs_to_fetch_this_batch = pending_object_ids[:gis_fetch_batch_size]
         arc_custom_params["objectIds"] = ",".join([str(x) for x in objs_to_fetch_this_batch])
         pending_object_ids = pending_object_ids[gis_fetch_batch_size:]
+
         data = arc_gis_get(url, arc_custom_params)
+
         if len(data["features"]) < len(objs_to_fetch_this_batch):
             print(
                 f"WARNING: fewer parcels returned than requested. Got {len(data['features'])} objects. "
@@ -164,9 +162,9 @@ def parcel_data_fetcher(url, arc_custom_params, *, do_incremental, thru_data, sk
                 print(f"Error creating parcel object: {e}")
                 traceback.print_exc()
                 # Allow us to continue and try with more parcels
-                error_count += 1.0
+                error_count += 1
             if count % 50 == 0:
-                print(f"Writing batch of 50 parcels to DB (total so far: {count})")
+                print(f"Writing batch of {len(parcels)} parcels to DB (total so far: {count})")
                 try:
                     RawSantaAnaParcel.objects.bulk_create(
                         parcels,
@@ -193,93 +191,3 @@ def parcel_data_fetcher(url, arc_custom_params, *, do_incremental, thru_data, sk
         parcels = []
     print(f"DONE writing parcels to DB. Wrote {count} parcels. {error_count} errors.")
     return count
-
-
-# Extract data from an ArcGIS server with API calls. Support different cities and data types (parcels, zones, etc).
-# pipestage is defined in the ARCGIS_DATA_SOURCES dict. thru_data is data passed from previous stages into the
-# fetcher. This function writes the results of each stage to a file, can reuse previous fetches, and logs the
-# capture. The function returns the filename in which the data was written.
-def extract_from_arcgis_api(
-    geo: GeoEnum, datatype: GisDataTypeEnum, pipestage: int, thru_data=None, always_use_existing=False
-):
-    _____url = (
-        "https://www.ocgis.com/survey/rest/services/WebApps/ParcelFeatures/FeatureServer/0/query?"
-        "where=SiteAddress+LIKE+%27%25santa+ana%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&"
-        "inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=&"
-        "returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&havingClause=&gdbVersion="
-        "&historicMoment=&returnDistinctValues=false&returnIdsOnly=true&returnCountOnly=false&returnExtentOnly=false"
-        "&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false"
-        "&multipatchOption=xyFootprint&resultOffset=&resultRecordCount=&returnTrueCurves=false"
-        "&returnExceededLimitFeatures=false&quantizationParameters=&returnCentroid=false"
-        "&timeReferenceUnknownClient=false&sqlFormat=none&resultType=&featureEncoding=esriDefault"
-        "&datumTransformation=&f=pjson"
-    )
-    from lib.extract.arcgis.params import ARCGIS_DATA_SOURCES, DATA_DIR
-
-    stage_config = ARCGIS_DATA_SOURCES[geo][datatype][pipestage]
-    now = datetime.now(tz=ZoneInfo("America/Los_Angeles"))
-    pipe = ARCGIS_DATA_SOURCES[geo][datatype][pipestage]
-    geo_name = ARCGIS_DATA_SOURCES[geo]["geo_name"]
-    output_dir = DATA_DIR / geo.value / "shapes" / datatype.value / f"{pipestage}.{stage_config['name']}"
-    logfile = DATA_DIR / "elt-fetches.log"
-    log = open(logfile, "a")
-    assert output_dir.is_dir()
-    print(f"Pipe stage {pipestage}: {stage_config['name']}")
-
-    # Check if we have previously stored data for this pipestage. If so, ask user whether to re-use or fetch new data.
-    existing_files = sorted(output_dir.iterdir(), reverse=True)
-    if not existing_files:
-        print("No existing file found... Creating one.")
-        create_new_file = True
-        do_incremental = False
-        use_file = -1
-    else:
-        print("Found files:")
-        for i, f in enumerate(existing_files):
-            print(f"{i + 1}: {f.name}")
-        print(f"{len(existing_files) + 1}:Create new file")
-        prompt_options = [str(x[0]) for x in enumerate(existing_files, start=1)] + [str(len(existing_files) + 1)]
-        do_incremental = stage_config[
-            "is_incremental"
-        ]  # default to incremental if possible, overridden by user prompt
-        if stage_config["is_incremental"]:
-            print(f"{len(existing_files) + 2}:Incrementally add to latest file ({existing_files[0].name})")
-            prompt_options += [str(len(existing_files) + 2)]
-        if always_use_existing:
-            print("Using latest file (always_use_existing=True)")
-            create_new_file = False
-            use_file = 1
-        else:
-            use_file = int(Prompt.ask("Which file to use?", choices=prompt_options))
-            do_incremental = use_file == len(existing_files) + 2
-            create_new_file = use_file == len(existing_files) + 1
-
-    # Got user intention, either fetch new data, incrementally add to latest file, or load existing data
-    if create_new_file:
-        outfile = output_dir / f"{now.strftime('%y%m%d')}.jsonl"
-        logmsg = f"Fetched {geo_name} {datatype} {stage_config['name']}, wrote to:\n        {outfile}\n"
-    elif do_incremental:
-        outfile = existing_files[0]
-        logmsg = (
-            f"Fetched {geo_name} {datatype} {stage_config['name']}, wrote incrementally to:\n        {outfile}\n"
-        )
-    else:  # load existing data file (outfile is really "input file" in this case)
-        outfile = existing_files[use_file - 1]
-        logmsg = f"Using existing {geo_name} {datatype} {stage_config['name']} file:\n        {outfile}\n"
-
-    # Fetch data from API or from file
-    result_count = stage_config["fetcher_fn"](
-        stage_config["url"],
-        stage_config["custom_params"],
-        outfile,
-        do_incremental,
-        thru_data=thru_data,
-        use_existing_data=not create_new_file and not do_incremental,
-    )
-
-    logmsg = f"{now.strftime('%y%m%d %H:%M:%S')} :: {logmsg}        Extracted {result_count} objects.\n"
-    log.write(logmsg)
-    log.close()
-    print(logmsg)
-    # rich.print_json(data=data)
-    return outfile
